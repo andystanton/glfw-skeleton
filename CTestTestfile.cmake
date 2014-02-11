673916a763aa0101089618d24c17166d571a8070
Andy Stanton <andystanton@gmail.com>
2014-02-11 12:51:03 +0000

    Remove cmake files

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/stanta01/Development/glfw-skeleton
-# Build directory: /Users/stanta01/Development/glfw-skeleton
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(lib/glfw)
