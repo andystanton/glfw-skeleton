c18492d3271d9dd1af74426588e3f49d32c59f54
Andy Stanton <andystanton@gmail.com>
2014-02-11 12:46:49 +0000

    Add test target to make

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/stanta01/Development/glfw-skeleton
+# Build directory: /Users/stanta01/Development/glfw-skeleton
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(lib/glfw)
