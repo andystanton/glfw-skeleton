412e408b8c0ad072f503eed54fd612223e7b815a
Andy Stanton <andystanton@gmail.com>
2014-04-20 11:44:17 +0100

    add test and examples

---
+++ include/Multiplier.h
@@ -0,0 +1,13 @@
+#ifndef _EXAMPLE_MULTIPLIER
+#define _EXAMPLE_MULTIPLIER
+
+class Multiplier
+{
+public:
+    Multiplier();
+    ~Multiplier();
+    
+    int multiply(int, int);
+};
+
+#endif
---
+++ src/Adder.cpp
@@ -0,0 +1,13 @@
+#include "../include/Adder.h"
+
+Adder::Adder() {
+
+}
+
+Adder::~Adder() {
+    
+}
+
+int Adder::add(int a, int b) {
+    return a + b;
+}
