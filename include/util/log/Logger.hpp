970bd72da073feec45ac31e778c844749c470c48
andystanton <andystanton@gmail.com>
2015-01-22 23:26:46 +0000

    add latest logging from janus

--- include/util/log/Logger.hpp
+++ include/util/log/Logger.hpp
@@ -4,26 +4,28 @@
 #include <iostream>
 #include <sstream>
 #include <regex>
+#include <map>
 
 using std::string;
 using std::regex;
 using std::stringstream;
 using std::cout;
+using std::map;
+using std::to_string;
 
 class LoggerFactory;
 
 class Logger
 {
-friend class LoggerFactory;
+    friend class LoggerFactory;
 public:
-    enum LoggerMode
-    {
-        ok,
-        fail,
-        endl
-    };
+    enum LoggerColour   { black, red, green, yellow, blue, magenta, cyan, white };
+    enum LoggerPreset   { ok, fail };
+    enum LoggerMode     { bold, reset, endl };
 
     Logger & operator<< (const string & message);
+    Logger & operator<< (const LoggerColour);
+    Logger & operator<< (const LoggerPreset);
     Logger & operator<< (const LoggerMode);
 
     ~Logger();
@@ -34,9 +36,21 @@ private:
     stringstream logEntry;
     regex colourRegex;
     bool noColour;
+    bool isBright;
 
+    string decorateString(const string &, const LoggerColour);
+    string filterColours(const string &);
+
+    string withColour(const LoggerColour);
+    string withMode(const LoggerMode);
+    string withPreset(const LoggerPreset);
+
+    string getHeader();
+
+    static const unsigned int DEFAULT_MAX_NAME_LENGTH;
     static const string COLOUR_PATTERN;
-    static const string MESSAGE_OK;
-    static const string MESSAGE_FAIL;
+    static const string COLOUR_TEMPLATE;
+    static const map<const LoggerColour, const int> COLOUR_MAP;
+    static const map<const LoggerMode, const string> MODE_MAP;
 };
 
